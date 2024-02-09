bc071b7f802fdc9ae7690a7f858494b304ef52d3
Mikkel Weinreich Olsen <mikkelo.privat@gmail.com>
2024-02-09 06:37:57 +0100

    hover animations and explainings to tools

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,10 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      visibility: ["group-hover"],
+    },
   },
   plugins: [],
-  darkMode: 'class'
+  darkMode: "class"
 }
