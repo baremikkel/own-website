ce9cbbb351345dec8ae56577dddee8a37f68ed8e
Mikkel Weinreich Olsen <mikkelo.privat@gmail.com>
2024-03-04 15:35:35 +0100

    done fucking dieco

--- src/background.js
+++ src/background.js
@@ -1,38 +1,12 @@
-document.addEventListener("DOMContentLoaded", function() {
-    // Folder path where your images are stored
-    var imagePath = "images/";
-
-    // Function to fetch images from the specified folder
-    function fetchImages(callback) {
-        var xhr = new XMLHttpRequest();
-        xhr.open("GET", imagePath, true);
-        xhr.onload = function() {
-            if (xhr.status == 200) {
-                var images = [];
-                var parser = new DOMParser();
-                var xmlDoc = parser.parseFromString(xhr.responseText, "text/xml");
-                var files = xmlDoc.getElementsByTagName("a");
-                for (var i = 0; i < files.length; i++) {
-                    var file = files[i].getAttribute("href");
-                    if (file.match(/\.(jpe?g|png|gif)$/)) {
-                        images.push(imagePath + file);
-                    }
-                }
-                callback(images);
-            }
-        };
-        xhr.send();
-    }
-
-    // Function to set a random background image
-    function setBackground(images) {
-        var randomIndex = Math.floor(Math.random() * images.length);
-        var backgroundElement = document.querySelector(".background");
-        backgroundElement.style.backgroundImage = "url('" + images[randomIndex] + "')";
-    }
-
-    // Fetch images and set background
-    fetchImages(function(images) {
-        setBackground(images);
-    });
-});
+window.onload = function() {
+    // Array of image filenames
+    var images = ["bg-img-1.jpg", "bg-img-2.jpg", "bg-img-3.jpg", "bg-img-4.jpg", "bg-img-5.jpg", "bg-img-6.jpg", "bg-img-7.jpg", "bg-img-8.jpg", "bg-img-9.jpg", "bg-img-10.jpg", "bg-img-11.jpg", "bg-img-12.jpg", "bg-img-13.jpg", "bg-img-14.jpg", "bg-img-15.jpg", "bg-img-16.jpg"];
+    
+    // Select a random image from the array
+    var randomIndex = Math.floor(Math.random() * images.length);
+    var randomImage = images[randomIndex];
+    
+    // Set the random image as the background
+    var backgroundDiv = document.querySelector('.background');
+    backgroundDiv.style.backgroundImage = "url('images/bg/" + randomImage + "')";
+};
